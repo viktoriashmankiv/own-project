9ef1ebf949098b62e468724a823d86674ff85deb
viktoriashmankiv <viktoriia.shmankiv@oa.edu.ua>
2020-01-20 14:37:04 +0000

    Create a code

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 const clockContainer = document.querySelector('.clock')
 setInterval(() => clockContainer.innerText = new Date().toLocaleTimeString(), 1000);
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Organic cosmetics shop';
